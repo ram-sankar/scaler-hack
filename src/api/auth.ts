15fecf7a6b1a4b28f60e7cac635fde62c7f109f0
unknown <=>
2022-03-27 07:57:17 +0530

    Ran Thanos-js

--- src/api/auth.ts
+++
@@ -1,5 +0,0 @@
-import client from "./client";
-
-const login = (email: string, password: string) => client.post("/api/auth", {email, password});
-
-export default { login }
--- src/api/user.ts
+++
@@ -1,5 +0,0 @@
-import client from "./client";
-
-const register = (data: Object) => client.post("/api/users", data);
-
-export default { register }
